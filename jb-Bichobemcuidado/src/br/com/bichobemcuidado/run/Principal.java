3f2a3a7e4078ae748cef371a9e377148a9bb24aa
Leandro Silva <leandro.tecnicoinfo@gmail.com>
2020-10-17 15:56:42 -0300

    Experimentando classe Scanner

--- jb-Bichobemcuidado/src/br/com/bichobemcuidado/run/Principal.java
+++ jb-Bichobemcuidado/src/br/com/bichobemcuidado/run/Principal.java
@@ -1,7 +1,13 @@
 package br.com.bichobemcuidado.run;
 
+import java.util.Scanner;
+
 public class Principal {
 	public static void main(String[] args) {
-
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite do Dono do Animal");
+		String nomeDoDono = scan.nextLine();
+		System.out.println("Dono do Bichinho: " + nomeDoDono);
 	}
 }
